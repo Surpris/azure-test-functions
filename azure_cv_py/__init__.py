9d301e5e23cb382f346297528d26c282c866e002
Toshiyuki NH <take90-it09-easy27@outlook.jp>
2023-09-14 17:38:27 +0900

    Update __init__.py
    import functions

--- azure_cv_py/__init__.py
+++ azure_cv_py/__init__.py
@@ -1,1 +1,11 @@
+"""
+azure_cv_py.py
+"""
 
+from .utils import set_key, set_endpoint
+from .constants import AZURE_COMPUTER_VISION_VERSION
+from .ocr import (
+    extract_characters_with_azure_cv_ocr,
+    extract_full_text,
+    extract_and_save_full_text_with_azure_cv_ocr
+)
