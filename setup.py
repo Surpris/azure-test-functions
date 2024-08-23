96888de7f1f8ec96d7f41765f8b1f8271b6ea47c
Toshiyuki NH <take90it9-easy27@outlook.jp>
2024-08-23 22:02:38 +0900

    rename

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ from setuptools import setup, find_packages
 AUTHOR_NAME = 'Surpris'
 
 setup(
-    name='azure-cv-py',
+    name='azure-test-functions',
     version='0.1.0',
     description='Functions using the Azure Computer Vision',
-    long_description=open('README.md').read(),
+    long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    url=f'https://github.com/{AUTHOR_NAME}/azure-cv-functions',
+    url=f'https://github.com/{AUTHOR_NAME}/azure-test-functions',
     author=AUTHOR_NAME,
     author_email='take90-it09-easy27@outlook.jp',
     license='Apache-2.0 License',
