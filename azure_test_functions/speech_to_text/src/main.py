c129955146114bffa8f8f1306dcec3a70b78bdd8
Toshiyuki NH <take90it9-easy27@outlook.jp>
2025-04-24 21:21:31 +0900

    feature: add a list-lang flag to list up the supported languages

--- azure_test_functions/speech_to_text/src/main.py
+++ azure_test_functions/speech_to_text/src/main.py
@@ -306,12 +306,19 @@ if __name__ == "__main__":
     parser.add_argument(
         "--fast_mode", dest="fast_mode", action="store_true"
     )
+    parser.add_argument(
+        "--list-lang", dest="list_lang", action="store_true",
+        help="list the supported languages"
+    )
     args = parser.parse_args()
     if args.la not in SUPPORTED_LANGUAGES:
         raise ValueError(
             f"language '{args.la}' is not supported. "
             f"supported languages are {SUPPORTED_LANGUAGES}."
         )
+    if args.list_lang:
+        print("supported languages:", SUPPORTED_LANGUAGES)
+        exit(0)
     if args.fast_mode:
         print("use the fast transcription API.")
     main(args.src, args.la, args.fast_mode)
